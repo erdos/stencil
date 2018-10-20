22437b3de777b9d7e9bbd047e741fab98579271b
Janos Erdos <janos.erdos@dbx.hu>
2018-10-20 15:34:31 +0200

    fix java tests too

--- src/stencil/postprocess/whitespaces.clj
+++ src/stencil/postprocess/whitespaces.clj
@@ -18,6 +18,7 @@
     xml-tree))
 
 (def ooxml-t :xmlns.http%3A%2F%2Fschemas.openxmlformats.org%2Fwordprocessingml%2F2006%2Fmain/t)
+(def ooxml-attr-space :xmlns.http%3A%2F%2Fwww.w3.org%2FXML%2F1998%2Fnamespace/space)
 
 (defn- should-fix?
   "We only fix <t> tags where the enclosed string starts or ends with whitespace."
@@ -30,7 +31,7 @@
         (.startsWith (str (last (:content element))) " ")))))
 
 (defn- fix-elem [element]
-  (assoc-in element [:attrs "xml:space"] "preserve"))
+  (assoc-in element [:attrs ooxml-attr-space] "preserve"))
 
 (defn fix-whitespaces [xml-tree]
   (postwalk-xml #(if (should-fix? %) (fix-elem %) %) xml-tree))
