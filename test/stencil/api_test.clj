00f1ffa7137cb11a8847887ecd07028324da6860
Janos Erdos <janos.erdos@dbx.hu>
2018-10-21 15:50:29 +0200

    added support for PPTX files

--- test/stencil/api_test.clj
+++ test/stencil/api_test.clj
@@ -1,9 +1,21 @@
 (ns stencil.api-test
   (:require [stencil.api :refer :all]))
 
-(comment (def template-1 (prepare "/home/erdos/Joy/stencil/test-resources/test-control-conditionals.docx"))
+(comment ;; try to prepare then render a DOCX file
 
-         (defn render-template-1 [output-file data]
-           (render! template-1 data :output output-file))
+  (def template-1 (prepare "/home/erdos/Joy/stencil/test-resources/test-control-conditionals.docx"))
 
-         (render-template-1 "/tmp/output-3.docx" {"customerName" "John Doe"}))
+  (defn render-template-1 [output-file data]
+    (render! template-1 data :output output-file))
+
+  (render-template-1 "/tmp/output-3.docx" {"customerName" "John Doe"}))
+
+(comment ;; try to prepare then render a PPTX presentation file
+
+  (def template-2 (prepare "/home/erdos/example-presentation.pptx"))
+
+  (defn render-template-2 [output-file data]
+    (render! template-2 data :output output-file))
+
+  (render-template-2 "/tmp/output-7.pptx"
+                     {"customerName" "John Doe" "x" "XXX" "y" "yyyy"}))
