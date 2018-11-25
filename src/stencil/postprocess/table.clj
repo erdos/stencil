29fb63d337424d0a8d7f9c90dac915b06ea7432c
janos erdos <erdos@localhost.localdomain>
2018-11-25 15:24:13 +0100

    saved 1 byte

--- src/stencil/postprocess/table.clj
+++ src/stencil/postprocess/table.clj
@@ -102,7 +102,7 @@
         current-cell-width (cell-width cell)
         cells-before (filter loc-cell? (next (iterations zip/left cell)))
         before-sum   (reduce + (map cell-width cells-before))]
-    (set (for [i (range current-cell-width)] (+ before-sum i)))))
+    (set (range before-sum (+ before-sum current-cell-width)))))
 
 (defn remove-columns
   "Egy adott row alatt az adott sorszamu oszlopokat eltavolitja.
