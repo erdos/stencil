9e294de96c70e79e80fcf5e77bee2ac1c42d969f
janos erdos <erdosj@gmail.com>
2019-02-08 16:21:56 +0100

    more unit tests

--- java-test/io/github/erdos/stencil/functions/NumberFunctionsTest.java
+++ java-test/io/github/erdos/stencil/functions/NumberFunctionsTest.java
@@ -11,25 +11,41 @@ public class NumberFunctionsTest {
     @Test
     public void testRound() {
         assertNull(ROUND.call((Object) null));
+
         assertEquals(1L, ROUND.call(0.9));
         assertEquals(1L, ROUND.call(1.1));
         assertEquals(2L, ROUND.call(1.5));
         assertEquals(1L, ROUND.call(1.0));
+
+        assertEquals(-1L, ROUND.call(-0.9));
+        assertEquals(-1L, ROUND.call(-1.1));
+        assertEquals(-1L, ROUND.call(-1.5));
+        assertEquals(-1L, ROUND.call(-1.0));
     }
 
     @Test
     public void testCeil() {
         assertNull(CEIL.call((Object) null));
+
         assertEquals(1L, CEIL.call(0.9));
         assertEquals(2L, CEIL.call(1.1));
         assertEquals(1L, CEIL.call(1.0));
+
+        assertEquals(-0L, CEIL.call(-0.9));
+        assertEquals(-1L, CEIL.call(-1.1));
+        assertEquals(-1L, CEIL.call(-1.0));
     }
 
     @Test
     public void testFloor() {
         assertNull(FLOOR.call((Object) null));
+
         assertEquals(0L, FLOOR.call(0.9));
         assertEquals(1L, FLOOR.call(1.1));
         assertEquals(1L, FLOOR.call(1.0));
+
+        assertEquals(-1L, FLOOR.call(-0.9));
+        assertEquals(-2L, FLOOR.call(-1.1));
+        assertEquals(-1L, FLOOR.call(-1.0));
     }
 }
