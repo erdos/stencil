7ffd3443ebe70cf18fd92b919da7926ee30cabd3
janos erdos <erdosj@gmail.com>
2019-02-25 11:32:12 +0100

    improvements

--- java-src/io/github/erdos/stencil/Main.java
+++ java-src/io/github/erdos/stencil/Main.java
@@ -1,67 +1,23 @@
 package io.github.erdos.stencil;
 
 import io.github.erdos.stencil.standalone.ArgsParser;
-import io.github.erdos.stencil.standalone.Parser;
+import io.github.erdos.stencil.standalone.StandaloneApplication;
 import io.github.erdos.stencil.standalone.StencilArgsParser;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Optional;
 
-import static io.github.erdos.stencil.API.prepare;
-import static io.github.erdos.stencil.API.render;
 import static io.github.erdos.stencil.impl.ClojureHelper.callShutdownAgents;
-import static io.github.erdos.stencil.impl.FileHelper.removeExtension;
-import static io.github.erdos.stencil.standalone.Parser.maybeDataFileFormat;
 
 public class Main {
 
     public static void main(String... args) throws IOException {
+        final ArgsParser.ParseResult parsed = StencilArgsParser.parse(args);
+        final StandaloneApplication app = new StandaloneApplication(parsed);
 
-        ArgsParser.ParseResult parsed = StencilArgsParser.parse(args);
+        // run application
+        app.run();
 
-        final File outputDir = StencilArgsParser.getOutputDirectory(parsed);
-
-        final Iterator<String> rest = parsed.getRestArgs().iterator();
-
-        for (String file : parsed.getRestArgs()) {
-            if (!new File(file).exists()) {
-                throw new IllegalArgumentException("File does not exist: " + file);
-            }
-            // TODO: also check that this is a readable data file.
-        }
-
-        while (rest.hasNext()) {
-            final File templateFile = new File(rest.next()).getAbsoluteFile();
-            final PreparedTemplate template = prepare(templateFile);
-
-            while (rest.hasNext()) {
-                final File dataFile = new File(rest.next()).getAbsoluteFile();
-                final Optional<Parser.DataFileFormat> format = maybeDataFileFormat(dataFile);
-                if (!format.isPresent()) {
-                    throw new IllegalArgumentException("Unknown extension, only JSON and EDN are supported: " + dataFile);
-                }
-
-                final Object data = format.get().parse(dataFile);
-                if (!(data instanceof Map)) {
-                    throw new IllegalArgumentException("Template data is not a map in file: " + dataFile + " of " + data.getClass());
-                } else {
-                    final TemplateData templateData = TemplateData.fromMap((Map) data);
-                    final EvaluatedDocument document = render(template, templateData);
-
-                    final File targetFile = targetFile(outputDir, templateFile, dataFile);
-                    document.writeToFile(targetFile);
-                }
-
-            }
-        }
-        
+        // stop Clojure thread pools
         callShutdownAgents();
-    }
-
-    private static File targetFile(File targetDirectory, File template, File data) {
-        return new File(targetDirectory, template.getName() + "-" + removeExtension(data) + ".docx");
     }
 }
--- java-src/io/github/erdos/stencil/standalone/StandaloneApplication.java
+++ java-src/io/github/erdos/stencil/standalone/StandaloneApplication.java
@@ -1,9 +1,74 @@
 package io.github.erdos.stencil.standalone;
 
+import io.github.erdos.stencil.EvaluatedDocument;
+import io.github.erdos.stencil.PreparedTemplate;
+import io.github.erdos.stencil.TemplateData;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+
+import static io.github.erdos.stencil.API.prepare;
+import static io.github.erdos.stencil.API.render;
+import static io.github.erdos.stencil.impl.FileHelper.extension;
+import static io.github.erdos.stencil.impl.FileHelper.removeExtension;
+import static io.github.erdos.stencil.standalone.Parser.maybeDataFileFormat;
+
 public class StandaloneApplication {
 
-    public void run(ArgsParser args) {
+    private final ArgsParser.ParseResult parsed;
+    private final File outputDir;
 
+    public StandaloneApplication(ArgsParser.ParseResult parsed) {
+        this.parsed = parsed;
+        this.outputDir = StencilArgsParser.getOutputDirectory(parsed);
     }
 
+    private void checkRestFilesExist() {
+        for (String file : parsed.getRestArgs()) {
+            if (!new File(file).exists()) {
+                throw new IllegalArgumentException("File does not exist: " + file);
+            }
+            // TODO: also check that this is a readable data file.
+        }
+    }
+
+    public void run() throws IOException {
+        checkRestFilesExist();
+
+        final Iterator<String> rest = parsed.getRestArgs().iterator();
+
+        while (rest.hasNext()) {
+            final File templateFile = new File(rest.next()).getAbsoluteFile();
+            final PreparedTemplate template = prepare(templateFile);
+
+            while (rest.hasNext()) {
+                final File dataFile = new File(rest.next()).getAbsoluteFile();
+                final Optional<Parser.DataFileFormat> format = maybeDataFileFormat(dataFile);
+                if (!format.isPresent()) {
+                    throw new IllegalArgumentException("Unknown extension, only JSON and EDN are supported: " + dataFile);
+                }
+
+                final Object data = format.get().parse(dataFile);
+                if (!(data instanceof Map)) {
+                    throw new IllegalArgumentException("Template data is not a map in file: " + dataFile + " of " + data.getClass());
+                } else {
+                    final TemplateData templateData = TemplateData.fromMap((Map) data);
+                    final EvaluatedDocument document = render(template, templateData);
+
+                    final File targetFile = targetFile(outputDir, templateFile, dataFile);
+                    document.writeToFile(targetFile);
+                }
+            }
+        }
+    }
+
+    private static File targetFile(File targetDirectory, File template, File data) {
+        final String part1 = template.getName();
+        final String part2 = removeExtension(data);
+        final String ext = extension(template);
+        return new File(targetDirectory, part1 + "-" + part2 + "." + ext);
+    }
 }
