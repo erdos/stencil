101db52cfd8e1fbf348feabdb61c74d157e975d8
janos erdos <erdosj@gmail.com>
2019-03-12 12:07:23 +0100

    standalone: nicer error handling

--- java-src/io/github/erdos/stencil/Main.java
+++ java-src/io/github/erdos/stencil/Main.java
@@ -1,5 +1,7 @@
 package io.github.erdos.stencil;
 
+import io.github.erdos.stencil.exceptions.EvalException;
+import io.github.erdos.stencil.exceptions.ParsingException;
 import io.github.erdos.stencil.standalone.ArgsParser;
 import io.github.erdos.stencil.standalone.StandaloneApplication;
 import io.github.erdos.stencil.standalone.StencilArgsParser;
@@ -17,12 +19,33 @@ public class Main {
 
             app.run();
         } catch (IllegalArgumentException e) {
-            System.out.println("ERROR:");
-            System.out.println(e.getMessage());
+            System.out.println("Parameter error! See --help for usage information.");
+            printException(e);
             System.exit(2);
+        } catch (ParsingException e) {
+            System.out.println("Error parsing template!");
+            System.out.println("Expression: " + e.getExpression());
+            printException(e);
+            System.exit(3);
+        } catch (EvalException e) {
+            System.out.println("Error rendering template!");
+            printException(e);
+            System.exit(4);
         } finally {
             // stop Clojure thread pools
             callShutdownAgents();
+        }
+    }
+
+    private static void printException(Throwable e) {
+        if (e.getMessage() != null && !e.getMessage().trim().isEmpty()) {
+            System.out.println(e.getMessage());
+        } else {
+            System.out.print(e.getClass().getName());
+        }
+
+        for (Throwable t = e.getCause(); t != null; t = t.getCause()) {
+            System.out.println("- " + t.getClass().getName() + " " + t.getMessage());
         }
     }
 }
