c1add169f2d89e767db23ea07e4395af924e3a3d
janos erdos <erdosj@gmail.com>
2019-03-25 22:23:56 +0100

    sd

--- src/stencil/postprocess/html.clj
+++ src/stencil/postprocess/html.clj
@@ -66,7 +66,6 @@
                             {:tag ooxml/br :content []}
                             {:tag ooxml/t :content [(str text)]})))}))))
 
-;; splits enclosing t and r tags
 (defn- fix-html-chunk [chunk-loc]
   (assert (instance? HtmlChunk (zip/node chunk-loc)))
   (let [lefts (zip/lefts chunk-loc)
