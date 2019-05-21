1daac6eb3679d50c0a0209369cf8da2a48ebfb03
janos erdos <erdosj@gmail.com>
2019-05-21 18:48:25 +0200

    example: added multipart document example

---
+++ examples/Multipart Template/Main.java
@@ -0,0 +1,41 @@
+import io.github.erdos.stencil.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String... args) throws IOException {
+
+        // prepare template file
+        PreparedTemplate template = API.prepare(findFile("template.docx"));
+
+        // prepare header fragment
+        PreparedFragment header = API.fragment(findFile("header.docx"));
+        PreparedFragment footer = API.fragment(findFile("footer.docx"));
+
+        Map<String, PreparedFragment> fragments = new HashMap<>();
+        fragments.put("header", header);
+        fragments.put("footer", footer);
+
+        // assemble template data
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("companyName", "ACME Company");
+        dataMap.put("companyAddress", "3786 Farm Meadow Drive, Nashville TN, 37214");
+
+        TemplateData data = TemplateData.fromMap(dataMap);
+
+        // render template
+        EvaluatedDocument result = API.render(template, fragments, data);
+
+        // write generated document to a file
+        File output = new File("multipart-output.docx");
+        result.writeToFile(output);
+    }
+
+    private static File findFile(String name) {
+        return new File(Main.class.getResource(name).getFile());
+    }
+}
---
+++ examples/Multipart Template/README.md
@@ -0,0 +1,10 @@
+# Multipart document example
+
+In this example, the contents of the header and footer are stored in distinct template files. These templates
+may also refer dynamic content from the template data map.
+
+Using multipart templates are useful in many scenarios. For example, you can store addresses, company information
+or legal texts in separate documents. You can also reuse common elements across templates to make it easier to change
+them in the future.
+
+See the [Main.java](Main.java) file for usage.
