f737b22937eeb8ee286cef83f467bc4811bf5b4e
janos erdos <erdosj@gmail.com>
2019-05-30 10:44:39 +0200

    release 0.3.2

--- service/project.clj
+++ service/project.clj
@@ -1,10 +1,10 @@
-(defproject io.github.erdos/stencil-service "0.3.1"
+(defproject io.github.erdos/stencil-service "0.3.2"
   :description "Web service for the Stencil templating engine"
   :url "https://github.com/erdos/stencil"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.github.erdos/stencil-core "0.3.1"]
+                 [io.github.erdos/stencil-core "0.3.2"]
                  [http-kit "2.2.0"]
                  [ring/ring-json "0.4.0"]]
   :main stencil.service.core)
