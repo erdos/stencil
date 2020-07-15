b133256bd8c7dd5f68f2ebe9c6a30bd3f7b29e03
Janos Erdos <erdosj@gmail.com>
2020-07-15 22:22:17 +0200

    fix: handle windows file separators, release v0.3.8
    Co-authored-by: Bali Miklós <miklos.bali@dbx.hu>

--- service/project.clj
+++ service/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.github.erdos/stencil-core "0.3.8-SNAPSHOT"]
+                 [io.github.erdos/stencil-core "0.3.8"]
                  [http-kit "2.2.0"]
                  [ring/ring-json "0.4.0"]]
   :main stencil.service.core)
