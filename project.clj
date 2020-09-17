e9deaffa1c400efa3a47403ab86a0190a36e5e4f
Janos Erdos <erdosj@gmail.com>
2020-09-17 22:35:24 +0200

    feat: version comes from project.clj (#41)

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject io.github.erdos/stencil-core
-  #=(eval (-> "src/STENCIL_VERSION.txt" slurp .trim))
-
+(defproject io.github.erdos/stencil-core "0.3.10-SNAPSHOT"
   :url "https://github.com/erdos/stencil"
   :description       "Templating engine for office documents."
   :license {:name "Eclipse Public License - v 2.0"
