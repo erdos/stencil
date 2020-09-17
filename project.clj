9846a712df2df152a27aaadde382c97025f34a53
Janos Erdos <erdosj@gmail.com>
2020-09-17 22:33:58 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/stencil-core "0.3.9"
+(defproject io.github.erdos/stencil-core "0.3.10-SNAPSHOT"
   :url "https://github.com/erdos/stencil"
   :description       "Templating engine for office documents."
   :license {:name "Eclipse Public License - v 2.0"
