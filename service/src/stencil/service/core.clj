2c301c540c217e1e086c35e53e0d3be79002b4a2
janos erdos <erdos@localhost.localdomain>
2020-10-05 22:28:59 +0200

    log instead of print in service

--- service/src/stencil/service/core.clj
+++ service/src/stencil/service/core.clj
@@ -103,8 +103,8 @@
         template-dir ^File (get-template-dir)
         server (run-server app {:port http-port})]
     (log/info "Started listening on" http-port "serving" (str template-dir))
-    ;(log/info "Available template files: ")
+    (log/info "Available template files: ")
     (doseq [^File line (tree-seq #(.isDirectory ^File %) (comp next file-seq) template-dir)
             :when (.isFile line)]
-      (println (str (.relativize (.toPath template-dir) (.toPath line)))))
+      (log/info (str (.relativize (.toPath template-dir) (.toPath line)))))
     (while true (read-line))))
