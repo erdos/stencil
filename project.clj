9cda3be86a00d7b4d23900164f734cf44e933a1d
janos erdos <erdosj@gmail.com>
2021-05-20 20:55:33 +0200

    chore: reorder lein plugins

--- project.clj
+++ project.clj
@@ -13,11 +13,8 @@
   :pom-addition ([:properties ["maven.compiler.source" "8"] ["maven.compiler.target" "8"]])
   :pom-plugins [[org.apache.maven.plugins/maven-surefire-plugin "2.20"]]
   :main io.github.erdos.stencil.Main
-  :plugins [[lein-javadoc "0.3.0"]
-            [lein-cloverage "1.2.1"]
-            [lein-test-out "0.3.1"]]
-  :aliases      {"junit" ["with-profile" "test" "do" "test-out" "junit" "junit.xml"]
-                 "coverage" ["cloverage" "--codecov" "--exclude-call" "clojure.core/assert" "--exclude-call" "stencil.util/trace" "--exclude-call" "stencil.util/fail"]}
+  :aliases      {"junit" ["with-profile" "+test" "test-out" "junit" "junit.xml"]
+                 "coverage" ["with-profile" "+ci" "cloverage" "--codecov" "--exclude-call" "clojure.core/assert" "--exclude-call" "stencil.util/trace" "--exclude-call" "stencil.util/fail"]}
   :javadoc-opts {:package-names ["stencil"]
                  :additional-args ["-overview" "java-src/overview.html"
                                    "-top" "<style>kbd{background:#ddd}; a[title~=class], a[title~=interface], a[title~=enum]{text-decoration: underline; font-weight: bold} dd>code{background:#eee}</style>"]}
@@ -35,6 +32,10 @@
                     :dependencies [[junit/junit "4.12"]
                                    [org.xmlunit/xmlunit-core "2.5.1"]
                                    [hiccup "1.0.5"]]
+                    :plugins      [[lein-test-out "0.3.1"]]
                     :resource-paths    ["test-resources"]
                     :test-paths ["java-test"]
-                    }})
+                    }
+             :ci {:plugins [[lein-javadoc "0.3.0"]
+                            [lein-cloverage "1.2.1"]]
+                  }})
