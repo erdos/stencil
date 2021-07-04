6ef2d8ac9a592183f7bb6b4bb992cb7bd8980e7a
Janos Erdos <janos.erdos@wise.com>
2021-07-04 10:28:21 +0200

    wip

--- test/stencil/functions_test.clj
+++ test/stencil/functions_test.clj
@@ -18,7 +18,7 @@
   (is (= "hello 42" (call-fn "format" "hello %d" 42)))
   (is (= "hello 42" (call-fn "format" "hello %d" 42.0)))
   (testing "Character formatting"
-    (is (= "Hello x" (call-fn "format" "Hello null" nil)))
+    (is (= "Hello null" (call-fn "format" "Hello %c" nil)))
     (is (= "Hello x" (call-fn "format" "Hello %c" "x")))
     (is (= "Hello X" (call-fn "format" "Hello %C" \x))))
   (testing "Indexed parameters"
