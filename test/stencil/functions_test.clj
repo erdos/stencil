31a9d6a7848cec1cf2afe0a480dba2293804f912
Janos Erdos <janos.erdos@wise.com>
2021-07-05 23:11:15 +0200

    formatting

--- test/stencil/functions_test.clj
+++ test/stencil/functions_test.clj
@@ -55,7 +55,8 @@
 
 (deftest test-replace-image
   (binding [stencil.model/*extra-files* (atom #{})]
-    (is (instance? ReplaceImage (call-fn "replaceImage" data-uri)))
+    (is (instance? ReplaceImage (call-fn "replaceImage" data-uri))))
+  (testing "Parsing errors"
     (is (thrown? ExceptionInfo (call-fn "replaceImage" nil)))
     (is (thrown? ExceptionInfo (call-fn "replaceImage" "not data uri")))
     (is (thrown? ExceptionInfo (call-fn "replaceImage" "data:image/unknown;base64,XXXXXXX")))
