4abf917ba03b208579e0b00757bc03556fe38e28
erdos <erdosj@gmail.com>
2022-04-11 22:47:36 +0200

    chore: start work on 0.4.1

--- service/project.clj
+++ service/project.clj
@@ -1,13 +1,13 @@
-(defproject io.github.erdos/stencil-service "0.4.0"
+(defproject io.github.erdos/stencil-service "0.4.1-SNAPSHOT"
   :description "Web service for the Stencil templating engine"
   :url "https://github.com/erdos/stencil"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha2"]
-                 [io.github.erdos/stencil-core "0.4.0"]
+                 [io.github.erdos/stencil-core "0.4.1-SNAPSHOT"]
                  [org.slf4j/slf4j-api "2.0.0-alpha5"]
                  [org.mozilla/rhino-engine "1.7.14"]
                  [http-kit "2.5.0"]
-                 [ring/ring-json "0.4.0"]]
+                 [ring/ring-json "0.4.1-SNAPSHOT"]]
   :aot :all
   :main stencil.service)
