a4b538ba4ad765616c01cf1172103b6d403c1b95
erdos <erdosj@gmail.com>
2022-07-25 09:07:27 +0200

    cleanup test

--- test/stencil/postprocess/fragments_test.clj
+++ test/stencil/postprocess/fragments_test.clj
@@ -5,18 +5,12 @@
             [stencil.util :refer [find-first-in-tree xml-zip]]
             [stencil.postprocess.fragments :refer :all]))
 
-;; make all private vars public!
-(let [target (the-ns 'stencil.postprocess.fragments)]
-  (doseq [[k v] (ns-map target)
-          :when (and (var? v) (= target (.ns ^clojure.lang.Var v)))]
-    (eval `(defn ~(symbol (str "-" k)) [~'& args#] (apply (deref ~v) args#)))))
+(defn- p [& xs] {:tag ooxml/p :content (vec xs)})
+(defn- r [& xs] {:tag ooxml/r :content (vec xs)})
+(defn- t [& xs] {:tag ooxml/t :content (vec xs)})
 
+(def -split-paragraphs @#'stencil.postprocess.fragments/split-paragraphs)
 
-(defn- p [& xs] {:tag ooxml/p :content xs})
-(defn- r [& xs] {:tag ooxml/r :content xs})
-(defn- t [& xs] {:tag ooxml/t :content xs})
-
-;; TODO: before-after are gone!
 (deftest test-split-paragraphs-1
   (let [data {:tag :root :content [(p (r (t "hello" :HERE) (t "vilag")))]}
         loc (find-first-in-tree #{:HERE} (xml-zip data))]
@@ -24,5 +18,8 @@
         (doto assert)
         (-split-paragraphs (p (r (t "one"))) (p (r (t "two"))))
         (zip/root)
-        ; (zip/node)
-        (->> (println :!!!)))))
+        (= {:tag :root
+             :content [(p (r (t "hello"))) ;; before
+                       (p (r (t "one"))) (p (r (t "two"))) ;; newly inserted
+                       (p (r (t) (t "vilag")))]}) ;; after
+        (is))))
