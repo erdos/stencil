a801b72628c2e348e58e6adfd54fa219028394b7
erdos <erdosj@gmail.com>
2022-11-18 14:33:19 +0100

    rm unused interface

--- java-test/io/github/erdos/stencil/IntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package io.github.erdos.stencil;
-
-
-/**
- * Annotates a test that needs a LibreOffice process in the background.
- */
-public interface IntegrationTest {
-    /* category marker */
-}
