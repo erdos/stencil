f0cba4b8d7cdccd54934337a3eb333916a560d30
erdos <erdosj@gmail.com>
2022-11-28 23:49:28 +0100

    asdf

--- src/stencil/grammar.clj
+++ src/stencil/grammar.clj
@@ -1,9 +1,9 @@
 (ns stencil.grammar)
 
-(defmacro expecting [pred]
-  `(fn [t#]
-     (when-let [v# (~pred (first t#))] 
-       [v# (next t#)])))
+(defn expecting [pred]
+  (fn [t]
+    (when-let [v (pred (first t))] 
+      [v (next t)])))
 
 #_
 (defn- chained [reader separator-reader]
@@ -18,30 +18,34 @@
       )
 
 
-(defmacro chained [reader separator]
-  `(fn f# [t#]
+(defn chained [reader separator]
+  (fn f [t]
        (when-let [fst# (reader (first t#))]
          (if-let [sep# (separator (second t#))]
            (when-let [thr# (reader (third t#))]
              (recur whatever)))
          )))
 
+;; right sides should be all functions.
+
+(defmacro grammar [bindings body]
+  `(letfn* [~@(for [[k v] (partition 2 bindings), x [k (list 'fn '[%] (list v '%))]] x)] ~body))
 
 (def grammar
-  (letfn* [powers      (chained expression (expecting #{:pow}))
-          mults       (chained powers (expecting #{:mul :div}))
-          adds        (chained mults  (expecting #{:plus :minus}))
-          parenthesed (all (expecting :open) expression (expecting #{:close}))
-          identifier  (expecting symbol?)
-          accessed    (all identifier :open-bracked expression :close-bracked)
-          fncall      (all (expecting :fncall) (chained expression #{:comma}) (:expectig :close))
-          string      (expecting string?)
-          number      (expecting number?)
-          value       (ether identifier string number)
+  (grammar [powers      (chained expression (expecting #{:pow}))
+            mults       (chained powers (expecting #{:mul :div}))
+            adds        (chained mults  (expecting #{:plus :minus}))
+            parenthesed (all (expecting :open) expression (expecting #{:close}))
+            identifier  (expecting symbol?)
+            accessed    (all identifier :open-bracked expression :close-bracked)
+            fncall      (all (expecting :fncall) (chained expression #{:comma}) (:expectig :close))
+            string      (expecting string?)
+            number      (expecting number?)
+            value       (ether identifier string number)
 
-          negated (all (optional (expecting #{:neg})) expression)
-          not     (all (optional (expecting #{:not})) negated)
-          power   (chained not (expecting #{:pow}))
-          expression  (either (parenthesed adds) adds accessed value)
+            negated (all (optional (expecting #{:neg})) expression)
+            not     (all (optional (expecting #{:not})) negated)
+            power   (chained not (expecting #{:pow}))
+            expression  (either (parenthesed adds) adds accessed value)
           ]
     expression))
