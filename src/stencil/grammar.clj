789700c15022004bc357b7ff2609e38c46483823
erdos <erdosj@gmail.com>
2022-11-28 23:44:59 +0100

    feat: declarative grammar

---
+++ src/stencil/grammar.clj
@@ -0,0 +1,47 @@
+(ns stencil.grammar)
+
+(defmacro expecting [pred]
+  `(fn [t#]
+     (when-let [v# (~pred (first t#))] 
+       [v# (next t#)])))
+
+#_
+(defn- chained [reader separator-reader]
+  (fn [tokens]
+    ()
+  ))
+
+#_
+(let [powers (chained xx (expecting #{:pow}))
+      mults  (chained powers (expecting #{:mul :div}))]
+      
+      )
+
+
+(defmacro chained [reader separator]
+  `(fn f# [t#]
+       (when-let [fst# (reader (first t#))]
+         (if-let [sep# (separator (second t#))]
+           (when-let [thr# (reader (third t#))]
+             (recur whatever)))
+         )))
+
+
+(def grammar
+  (letfn* [powers      (chained expression (expecting #{:pow}))
+          mults       (chained powers (expecting #{:mul :div}))
+          adds        (chained mults  (expecting #{:plus :minus}))
+          parenthesed (all (expecting :open) expression (expecting #{:close}))
+          identifier  (expecting symbol?)
+          accessed    (all identifier :open-bracked expression :close-bracked)
+          fncall      (all (expecting :fncall) (chained expression #{:comma}) (:expectig :close))
+          string      (expecting string?)
+          number      (expecting number?)
+          value       (ether identifier string number)
+
+          negated (all (optional (expecting #{:neg})) expression)
+          not     (all (optional (expecting #{:not})) negated)
+          power   (chained not (expecting #{:pow}))
+          expression  (either (parenthesed adds) adds accessed value)
+          ]
+    expression))
