a260170691584f07c3fcc72185adb988d6c9292e
erdos <erdosj@gmail.com>
2022-12-07 22:25:28 +0100

    wip: demo language

--- src/stencil/grammar.clj
+++ src/stencil/grammar.clj
@@ -1,18 +1,16 @@
 (ns stencil.grammar)
 
-(defn- third [x] (nth x 2))
-
 (defn- expecting [pred]
   (fn [t]
     (when-some [v (pred (first t))] 
       [v (next t)])))
 
+(assert (= [true [:a]] ((expecting keyword?) [:k :a])))
+
 (defn- guarded [pred]
   (fn [t]
     (when (pred (first t))
       [(first t) (next t)])))
-
-(assert (= [true [:a]] ((expecting keyword?) [:k :a])))
 
 ;; left-associative chained infix expression
 (defn- chained [reader separator reducer]
@@ -20,11 +18,13 @@
     (when-let [[result tokens] (reader tokens)]
       (loop [tokens tokens
              result result]
-        (if-let [[fs tokens] (separator tokens)]
-          (if-let [[op2 tokens] (reader tokens)]
-            (recur tokens (reducer result fs op2))
-            (throw (ex-info "Unexpected charater!" {:tokens tokens})))
-          [result tokens])))))
+        (if (empty? tokens)
+          [result nil]
+          (if-let [[fs tokens] (separator tokens)]
+            (if-let [[op2 tokens] (reader tokens)]
+              (recur tokens (reducer result fs op2))
+              (throw (ex-info "Unexpected charater!" {:tokens tokens})))
+            [result tokens]))))))
 
 (assert (= [[[:a :+ :b] :+ :c] [:!!]]
            ((chained (expecting #{:a :b :c}) (expecting #{:+}) vector) [:a :+ :b :+ :c :!!])))
@@ -41,32 +41,35 @@
                 (reduced nil)))
       [[] tokens] readers)))
 
-#_
+(defn- either
+  ([a b] (fn [tokens] (or (a tokens) (b tokens))))
+  ([a b c] (fn [tokens] (or (a tokens) (b tokens) (c tokens))))
+  ([a b c d] (fn [t] (or (a t) (b t) (c t) (d t)))))
+
 (defmacro grammar [bindings body]
   `(letfn* [~@(for [[k v] (partition 2 bindings), x [k (list 'fn '[%] (list v '%))]] x)] ~body))
-
-
 
 (defn- parenthesed [reader]
   (all (expecting #{:open}) reader (expecting #{:close})))
 
-#_
-(def grammar
-  (grammar [powers      (chained expression (expecting #{:pow}) vector)
-            mults       (chained powers (expecting #{:mul :div}) vector)
-            adds        (chained mults  (expecting #{:plus :minus}) vector)
-            parenthesed (all (expecting :open) expression (expecting #{:close}))
-            identifier  (guarded symbol?)
-            accessed    (all identifier :open-bracked expression :close-bracked)
-            fncall      (all (expecting #{:fncall}) (chained expression #{:comma} vector) (expectig #{:close}))
-            string      (guarded string?)
-            number      (guarded number?)
-            value       (ether identifier string number)
+(defn unchain [p op p2] (list op p p2))
+; (defn unchainr [[op1 p1 p2] op2 p3] (list op1 p1 (list op2 p2 p3)))
 
-            negated (all (optional (expecting #{:neg})) expression)
-            not     (all (optional (expecting #{:not})) negated)
-            or      (chained not (expecting #{:or}))
-            and     (chained and (expecting #{:or}))
-            expression  (either (parenthesed adds) adds accessed value)
-          ]
-    expression))
+(def testlang
+  (grammar [num (either (guarded number?) (guarded symbol?) (guarded string?)) ;; add (parenthesed expression) here?
+            neg (chained num (expecting #{:neg}) unchain)
+            not (chained neg (expecting #{:not}) unchain)
+            pow (chained not (expecting #{:pow}) unchain) ;; TODO: right-associative
+            mul (chained pow (expecting '#{* /}) unchain)
+            add (chained mul (expecting '#{+ -}) unchain)
+
+            cmp  (chained add (expecting '#{< > <= >=}) unchain)
+            cmpe (chained cmp (expecting '#{= <>}) unchain) ;; eq/neq
+
+            and (chained cmpe (expecting '#{and}) unchain)
+            or  (chained and (expecting '#{or}) unchain)
+            expression or]
+           expression))
+
+(println (testlang '[123 + 344 + aaa * 34 + 1 = 34 or a < b]))
+
