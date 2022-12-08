4d27a4ec4caa4796b22b453f268b98ece457b134
erdos <erdosj@gmail.com>
2022-12-08 20:19:05 +0100

    wip

--- src/stencil/grammar.clj
+++ src/stencil/grammar.clj
@@ -26,20 +26,17 @@
               (throw (ex-info "Unexpected charater!" {:tokens tokens})))
             [result tokens]))))))
 
-(assert (= [[[:a :+ :b] :+ :c] [:!!]]
-           ((chained (expecting #{:a :b :c}) (expecting #{:+}) vector) [:a :+ :b :+ :c :!!])))
+;(assert (= [[[:a :+ :b] :+ :c] [:!!]]
+;           ((chained (expecting #{:a :b :c}) (expecting #{:+}) vector) [:a :+ :b :+ :c :!!])))
 
-;; right sides should be all functions.
-
-;; parses all readers or none
-(defn- all [& readers]
-;; TODO: if one has been read then it should thow error at that point
+(defn- all [condition & readers]
   (fn [tokens]
-    (reduce (fn [[result tokens] reader]
-              (if-let [[r tokens] ()]
-                [(conj result r) tokens]
-                (reduced nil)))
-      [[] tokens] readers)))
+    (when-let [[result tokens] (condition tokens)]
+      (reduce (fn [[result tokens] reader]
+                (if-let [[r tokens] (reader tokens)]
+                  [(conj result r) tokens]
+                  (throw (ex-info "Could not read!" {:reader reader :prefix tokens}))))
+              [[result] tokens] readers))))
 
 (defn- either
   ([a b] (fn [tokens] (or (a tokens) (b tokens))))
@@ -49,27 +46,74 @@
 (defmacro grammar [bindings body]
   `(letfn* [~@(for [[k v] (partition 2 bindings), x [k (list 'fn '[%] (list v '%))]] x)] ~body))
 
+(defn- mapping [reader mapper]
+  (fn [tokens]
+    (when-let [[result tokens] (reader tokens)]
+      [(mapper result) tokens])))
+
 (defn- parenthesed [reader]
-  (all (expecting #{:open}) reader (expecting #{:close})))
+  (mapping (all (expecting #{:open}) reader (expecting #{:close})) second))
+; (assert (= [23 nil] ((parenthesed (guarded number?)) [:open 23 :close])))
 
 (defn unchain [p op p2] (list op p p2))
-; (defn unchainr [[op1 p1 p2] op2 p3] (list op1 p1 (list op2 p2 p3)))
+
+(defn at-least-one [reader]
+  (fn [tokens]
+    (when-let [[result tokens] (reader tokens)]
+      (loop [tokens tokens, result [result]]
+        (if-let [[res tokens] (reader tokens)]
+          (recur tokens (conj result res))
+          [result tokens])))))
+
+(defn optional [reader] ;; always matches
+  (fn [tokens] (or (reader tokens) [nil tokens])))
 
 (def testlang
-  (grammar [num (either (guarded number?) (guarded symbol?) (guarded string?)) ;; add (parenthesed expression) here?
-            neg (chained num (expecting #{:neg}) unchain)
-            not (chained neg (expecting #{:not}) unchain)
-            pow (chained not (expecting #{:pow}) unchain) ;; TODO: right-associative
-            mul (chained pow (expecting '#{* /}) unchain)
-            add (chained mul (expecting '#{+ -}) unchain)
+  (grammar [val (either access-or-fncall
+                        (parenthesed expression)
+                        (guarded number?)
+                        (guarded string?))
+            iden (guarded symbol?)
 
+            bracketed   (mapping (all (expecting #{:open-bracket}) expression (expecting #{:close-bracket})) second)
+            args        (mapping (chained (all expression) (expecting #{:comma}) (fn [a _ c] (into a c))) (fn [x] [:b x]))
+            access      (mapping (at-least-one bracketed) (fn [a] [:a a]))
+            access-or-fncall (mapping (all iden (optional (either (parenthesed args) access)))
+                                (fn [[sym args]]
+                                  (case (first args)
+                                    nil sym
+                                    :a (list* :get sym (second args))
+                                    :b (list* :fncall sym (second args))
+                                  )))
+
+
+            ;; TODO: fn call and commas
+
+            neg (either (all (expecting '#{-}) val) val) ;; TODO; map
+            not (either (all (expecting #{:not}) neg) neg) ;; TODO: map
+
+            pow (chained not (expecting #{:pow}) unchain) ;; TODO: right-associative
+            mul (chained pow (expecting '#{* / %}) unchain)
+            add (chained mul (expecting '#{+ -}) unchain)
             cmp  (chained add (expecting '#{< > <= >=}) unchain)
             cmpe (chained cmp (expecting '#{= <>}) unchain) ;; eq/neq
-
-            and (chained cmpe (expecting '#{and}) unchain)
-            or  (chained and (expecting '#{or}) unchain)
+            and (chained cmpe (expecting #{:and}) unchain)
+            or  (chained and (expecting #{:or}) unchain)
             expression or]
            expression))
 
-(println (testlang '[123 + 344 + aaa * 34 + 1 = 34 or a < b]))
+(defn runlang [grammar input]
+  (if-let [[result tokens] (grammar input)]
+    (if (empty? tokens)
+      result
+      (throw (ex-info "Expected EOF found characters" {})))
+    (throw (ex-info "Could not parse" {}))))
 
+
+(println (runlang testlang '[ 1 + 2 + 3]))
+
+(println (runlang testlang '[ hello :open-bracket idx + 1 :close-bracket :open-bracket 4 :close-bracket * 2]))
+(println (runlang testlang '[ :open 1 + 2 :close * 3]))
+(println (runlang testlang '[123 + 344 + aaa * 34 + 1 = 34 :or a < b]))
+
+(println (runlang testlang '[hello :open 1 :comma 2 :close]))
