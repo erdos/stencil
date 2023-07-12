183fb68157b51cef8a9eb0ac9760fd6f0d000ae4
erdos <erdosj@gmail.com>
2023-07-12 11:30:40 +0200

    feat: version 0.5.1

--- service/project.clj
+++ service/project.clj
@@ -1,10 +1,10 @@
-(defproject io.github.erdos/stencil-service "0.5.1-SNAPSHOT"
+(defproject io.github.erdos/stencil-service "0.5.1"
   :description "Web service for the Stencil templating engine"
   :url "https://github.com/erdos/stencil"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.github.erdos/stencil-core "0.5.1-SNAPSHOT"]
+                 [io.github.erdos/stencil-core "0.5.1"]
                  [org.slf4j/slf4j-api "2.0.0-alpha7"]
                  [org.mozilla/rhino-engine "1.7.14"]
                  [http-kit "2.5.0"]
