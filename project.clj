32c29cf0778ef8501d92cd723fcb8ea76d197240
erdos <erdosj@gmail.com>
2023-07-12 11:45:25 +0200

    chore: start work on v0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/stencil-core "0.5.1"
+(defproject io.github.erdos/stencil-core "0.5.2-SNAPSHOT"
   :url "https://github.com/erdos/stencil"
   :description       "Templating engine for office documents."
   :license {:name "Eclipse Public License - v 2.0"
