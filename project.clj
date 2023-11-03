0203957035e371384e621523acf66a3fca2903d7
janos erdos <erdosj@gmail.com>
2023-11-03 17:18:13 +0100

    feat: release version 0.5.6 to trigger docker build

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/stencil-core "0.5.5"
+(defproject io.github.erdos/stencil-core "0.5.6"
   :url "https://github.com/erdos/stencil"
   :description       "Templating engine for office documents."
   :license {:name "Eclipse Public License - v 2.0"
