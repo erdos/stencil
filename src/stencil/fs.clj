b4c4592d9e999dc4778173ee51e59de51fe3d872
janos erdos <erdosj@gmail.com>
2025-04-07 22:39:55 +0200

    feat: less reflection

--- src/stencil/fs.clj
+++ src/stencil/fs.clj
@@ -18,3 +18,13 @@
 
 ;; remove /../ parts
 (defn unroll [^File f] (-> f .toPath .normalize .toFile))
+
+(defn delete!
+  "Recursively delete a directory or a single file."
+  [^File f]
+  (FileHelper/forceDelete f))
+
+(defn ->tmp-file
+  "Create a new temporary file in the parent directory."
+  [^File parent prefix suffix]
+  (FileHelper/createNonexistentTempFile parent prefix suffix))
