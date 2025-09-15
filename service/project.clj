ec8cc5fc0e14383d7b78d4d0fa489b309ec65b44
Janos Erdos <erdosj@gmail.com>
2025-09-15 08:32:33 +0000

    chore: start work on v0.6.6

--- service/project.clj
+++ service/project.clj
@@ -1,10 +1,10 @@
-(defproject io.github.erdos/stencil-service "0.6.5"
+(defproject io.github.erdos/stencil-service "0.6.6-SNAPSHOT"
   :description "Web service for the Stencil templating engine"
   :url "https://github.com/erdos/stencil"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.github.erdos/stencil-core "0.6.5"]
+                 [io.github.erdos/stencil-core "0.6.6-SNAPSHOT"]
                  [org.slf4j/slf4j-api "2.0.9"]
                  [org.mozilla/rhino-engine "1.7.14"]
                  [http-kit "2.7.0"]
